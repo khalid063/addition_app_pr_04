ad2d99037893614cfa81426f5c9e363edb9ccb04
KhalidPC <khalid.ahmad063@gmail.com>
2023-06-17 20:28:55 +0600

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "app addition",
+      home: HomeScreen(),
     );
   }
 }
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("app bar"),
+      ),
+    );
+  }
+}
+
 
